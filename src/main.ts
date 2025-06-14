e04afe52d3445758019f03538514b281059a3ef2
Rider Mamani Paco <rdrmamani@gmail.com>
2025-06-14 17:40:13 -0400

    cors

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,8 @@ async function bootstrap() {
     }),
   );
 
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle('API de Reservas de Espacios Comunitarios')
     .setDescription(
